f9cc8bab406af7a19b45c48fadfc172b0ffb1b6d
Tortorelli Rocco <130174175+TheRocc-o@users.noreply.github.com>
2023-06-27 18:49:39 +0200

    Add files via upload

---
+++ EPICODE/M4-1.py
@@ -0,0 +1,13 @@
+studenti = 25 #1 esercizio
+print (studenti) #2 esercizio
+nuovistudenti = 3 #3 esercizio
+Studenti_complessivi = studenti + nuovistudenti #4 esercizio
+print (Studenti_complessivi)
+
+
+numeri = [0,1,2,3,4,5] #5 esercizio
+print (numeri)
+
+
+i_miei_film_preferiti = ['Django Unchained','Interstellar','Parasite'] #6 esercizio
+print(i_miei_film_preferiti)
